5f33dddfadb2b011fcac47cf8df68db9d3dc5a12
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2019-10-08 00:07:21 -0300

    feat(mongo): connect to the database

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const app = express();
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('common'));
+}
 app.use(express.json());
-app.use(morgan('common'));
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+// app.use(express.static(`${__dirname}/public`));
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+require('dotenv').config({ path: './config.env' });
+require('./logs/logger');
 const chalk = require('chalk');
 const app = require('./app');
-const logs = require('./logs/logger');
 
-const PORT = 7770;
+const PORT = process.env.PORT || 7770;
+
+// console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
-  console.log(chalk.blue(`\n Servidor `)+chalk.green(`Express `)+chalk.blue(`Rodando `)+chalk.red(`@ Porta ${PORT}! \n`));
+  console.log(
+    chalk.blue(`\n Servidor `)+
+    chalk.green(`Express `)+
+    chalk.blue(`Rodando `)+
+    chalk.red(`@ Porta ${PORT}! \n`
+    ));
 });
