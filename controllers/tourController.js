d1738327f67d73f81f3398cdda7a4fb609c24310
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2019-10-11 18:02:35 -0300

    feat(controller/tours): crud operations working

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,35 @@
 const Tour = require('./../models/tourModel');
 
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find()
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'SUCCESS',
-    results: tours.length,
-    data: { tours }
-  });
+    res.status(200).json({
+      status: 'SUCCESS',
+      results: tours.length,
+      data: { tours }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'FAIL',
+      message: err
+    });
+  }
 }
 
-exports.getTourById = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'SUCCESS',
-    data: { tour }	  
-  })
+exports.getTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    res.status(200).json({
+      status: 'SUCCESS',
+      data: { tour }	  
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'FAIL',
+      message: err
+    });
+  }
 }
 
 exports.createTour = async (req, res) => {
@@ -35,18 +49,38 @@ exports.createTour = async (req, res) => {
   }
 }
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+try {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
   res.status(200).json({
     status: 'SUCCESS',
     data: {
-      tour: 'Estamos desenvolvento essa churanha. Dá uma segurada aí.'
+      tour
     }
-  });
+  })
+} catch (err) {
+  res.status(400).json({
+    status: 'FAIL',
+    message: err
+  })
 }
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'SUCCESS',
-    data: null
-  });
+}
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'SUCCESS',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'FAIL',
+      message: err
+    })
+  }
 }
